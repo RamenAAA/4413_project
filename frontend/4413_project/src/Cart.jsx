ca8145c51be02a52bca505a7739da795df66f6bc
mrl3558 <115577522+mrl3558@users.noreply.github.com>
2024-07-29 20:14:24 -0400

    cart frontend implementation

--- frontend/4413_project/src/Cart.jsx
+++ frontend/4413_project/src/Cart.jsx
@@ -1,9 +1,28 @@
+import CartItems from "./CartItems";
 
 function Cart(){
     return(
         <div className="cart">
-            <h2 className="pageHeader">Cart</h2>
+            <h2 className="pageHeader">Shopping Cart</h2>
+            <div  className="cart-container">
+                <div className="cartItems">
+                    <CartItems></CartItems>
+                    <CartItems></CartItems>
+                    <CartItems></CartItems>
+                </div>
+                <div className="buy-container">
+                    <div className="buyText">
+                        <p>Item Number : 2</p>
+                        <h3><b>Order Total: $100.00</b></h3>
+                    </div>
+                    <button className="checkoutButton">Checkout</button>
+                </div>
+            </div>
+
+
         </div>
+
+        
     );
 }
 
---
+++ frontend/4413_project/src/CartItems.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function CartItems(){
+    return(
+        <div className="cartItem">
+            <h3>Cart Item</h3>
+            <img src="https://via.placeholder.com/150" />
+
+            
+
+        </div>
+
+        
+    );
+}
+
+export default CartItems
